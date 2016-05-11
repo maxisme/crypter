dee6771777a624f816a798619cc90798dfe7c2ce
maxisme <max@maxis.me>
2016-05-11 18:14:36 +0100

    deleted:    Firefox Extension/@facebookchatencrypter-0.0.91.xpi 	deleted:    Firefox Extension/@facebookchatencrypter-0.0.92.xpi 	deleted:    Firefox Extension/createXPI 	deleted:    Firefox Extension/data/bililiteRange.js 	deleted:    Firefox Extension/testExt 	deleted:    Firefox extension/data/aes.js 	deleted:    Firefox extension/data/background.js 	deleted:    Firefox extension/data/blurbox.js 	deleted:    Firefox extension/data/jquery.min.js 	deleted:    Firefox extension/index.js 	deleted:    Firefox extension/package.json 	deleted:    Safari Extension/Crypter.safariextension/Info.plist 	deleted:    Safari Extension/Crypter.safariextension/aes.js 	deleted:    Safari Extension/Crypter.safariextension/background.js 	deleted:    Safari Extension/Crypter.safariextension/bililiteRange.js 	deleted:    Safari Extension/Crypter.safariextension/blurbox.js 	deleted:    Safari Extension/Crypter.safariextension/images/335.gif 	deleted:    Safari Extension/Crypter.safariextension/images/lock_empty.png 	deleted:    Safari Extension/Crypter.safariextension/images/lock_encrypted.png 	deleted:    Safari Extension/Crypter.safariextension/images/lock_open.png 	deleted:    Safari Extension/Crypter.safariextension/images/lock_should_black.png 	deleted:    Safari Extension/Crypter.safariextension/images/tool.png 	deleted:    Safari Extension/Crypter.safariextension/images/tool.svg 	deleted:    Safari Extension/Crypter.safariextension/jquery.min.js 	deleted:    Safari Extension/Crypter.safariextension/logo.png 	deleted:    Safari Extension/safari_extension.cer 	modified:   chrome.zip

--- Firefox extension/index.js
+++
@@ -1,7 +0,0 @@
-var self = require('sdk/self');
-var pageMod = require("sdk/page-mod");
-
-pageMod.PageMod({
-  include: "*.facebook.com",
-  contentScriptFile: [ "./jquery.min.js", "./aes.js", "./blurbox.js", "./background.js" ]
-});
